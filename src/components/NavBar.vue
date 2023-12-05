f517ad8ec936992d24b23a104ae8709f5b0bc7e2
py8141 <parth.yadav8141@gmail.com>
2023-12-05 17:23:04 +0530

    cartStore

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,7 +30,10 @@ export default defineComponent({
         return {
             userIcon,
             ordericon,
-            shopingcart
+            shopingcart,
+            isLoggedIn,
+            logout,
+            takeMeHome
         }
     },
 })
---
+++ src/store/order-store.js
@@ -0,0 +1,3 @@
+import { defineStore } from "pinia";
+import { reactive } from "vue";
+
